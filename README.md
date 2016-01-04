fc1b5c8a1e80e2e72ae47b9592ab5392ec6084a5
Rick G <lacunainc04@gmail.com>
2016-01-03 18:23:18 -0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ncw-api-generator
-generator for APIs
+# Design Inspiration Content Organizer
+This is a rails application that helps us organize the api content for the NCW Design Inspiration Site.
+
+## Installation
+bundle install
+rake db:migrate && rake db:seed
+
+# Run local server
+rails server
+In cloud9: rails server -b $IP -p $PORT
--- db/schema.rb
+++ db/schema.rb
@@ -27,19 +27,19 @@ ActiveRecord::Schema.define(version: 20151221185713) do
   add_index "typographies", ["user_id"], name: "index_typographies_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.boolean  "admin"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
     t.boolean  "uploader",               default: true
   end
 

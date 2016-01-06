e4b39909da0f5e71c71bff8f76b3d50aeb2ae893
Rick G <lacunainc04@gmail.com>
2016-01-06 02:13:56 -0800

    Add presence validation on :title and :image

--- app/models/typography.rb
+++ app/models/typography.rb
@@ -2,4 +2,7 @@ class Typography < ActiveRecord::Base
   belongs_to :user 
   mount_uploader :image, ImageUploader 
   # Found at app/uploaders/image_uploader.rb
+  
+  validates :title, presence: true
+  validates :image, presence: true
 end
